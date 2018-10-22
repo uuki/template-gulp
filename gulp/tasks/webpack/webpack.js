8b824c720a2c1323ddca86675dddd5994f6accde
Yuichi Aoki <sena.rn68@gmail.com>
2018-10-22 15:25:14 +0900

    modify webpack task

--- gulp/tasks/webpack/webpack.js
+++ gulp/tasks/webpack/webpack.js
@@ -6,7 +6,10 @@ const webpackConfig = require('../../../webpack.config');
 const config = require('../../../config')['webpack'];
 
 gulp.task('webpack', () => {
-  return webpackStream(webpackConfig, webpack)
-    .pipe(process.$.plumber())
-    .pipe(gulp.dest(config.publishDir));
+  return gulp.src(`${ config.sourceDir }/${ config.entry }`)
+  .pipe(webpackStream(webpackConfig, webpack))
+  .on('error', function(error) {
+    this.emit('end')
+  })
+  .pipe(gulp.dest(config.publishDir));
 })
